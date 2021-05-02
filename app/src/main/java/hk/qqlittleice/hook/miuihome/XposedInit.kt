f63424d754d0c5e8ada866f991fe59e2be011ddd
QQLittleice <1767523953@qq.com>
2021-05-02 19:47:19 +0800

    Add Res Hook

--- app/src/main/java/hk/qqlittleice/hook/miuihome/XposedInit.kt
+++ app/src/main/java/hk/qqlittleice/hook/miuihome/XposedInit.kt
@@ -3,18 +3,28 @@ package hk.qqlittleice.hook.miuihome
 import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.content.res.XModuleResources
 import androidx.annotation.Keep
-import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.*
+import de.robv.android.xposed.callbacks.XC_InitPackageResources
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import hk.qqlittleice.hook.miuihome.utils.ktx.hookAfterAllMethods
 
 @Keep
-class XposedInit : IXposedHookLoadPackage {
+class XposedInit : IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit {
 
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
-        if (lpparam?.packageName != Config.hookPackage) return
+    companion object {
+        const val TYPE_string = "string"
+        const val TYPE_color = "color"
+        const val TYPE_dimen = "dimen"
+        const val TYPE_integer = "integer"
+        const val TYPE_bool = "bool"
+    }
+
+    private lateinit var modulePath: String
+
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName != Config.hookPackage) return
         XposedHelpers.findAndHookMethod(Application::class.java, "attach", Context::class.java, object : XC_MethodHook() {
             override fun afterHookedMethod(param: MethodHookParam) {
                 HomeContext.context = param.args[0] as Context
@@ -26,6 +36,16 @@ class XposedInit : IXposedHookLoadPackage {
                 MainHook().doHook()
             }
         })
+    }
+
+    override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
+        modulePath = startupParam.modulePath
+    }
+
+    override fun handleInitPackageResources(resparam: XC_InitPackageResources.InitPackageResourcesParam) {
+        if (resparam.packageName != Config.hookPackage) return
+        val modRes = XModuleResources.createInstance(modulePath, resparam.res)
+        resparam.res.apply {}
     }
 
 }
