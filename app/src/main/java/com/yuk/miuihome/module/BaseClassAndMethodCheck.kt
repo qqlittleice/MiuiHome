089adafe59ff7a7b325949ae4068e962118295fc
QQ littleice <1767523953@qq.com>
2021-09-14 17:20:52 +0800

    Test Code

---
+++ app/src/main/java/com/yuk/miuihome/module/BaseClassAndMethodCheck.kt
@@ -0,0 +1,42 @@
+package com.yuk.miuihome.module
+
+import com.yuk.miuihome.HomeContext
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedHelpers.ClassNotFoundError
+
+interface BaseClassAndMethodCheck {
+
+    fun classAndMethodList(): ArrayList<String>
+
+    private fun getAllMethods(cls: Class<*>): ArrayList<String> {
+        val methodNameList = ArrayList<String>()
+        var loopCls = cls
+        while (loopCls != Any::class.java) {
+            for (item in cls.methods) {
+                methodNameList.add(item.name)
+            }
+            loopCls = cls.superclass
+        }
+        return methodNameList
+    }
+
+    fun checkClassAndMethodExist(): Boolean {
+        val list = classAndMethodList()
+        try {
+            for (i in 0 until list.size step 2) {
+                val cls = XposedHelpers.findClass(list[i], HomeContext.classLoader)
+                if (list[i+1] !in getAllMethods(cls)) {
+                    return false
+                }
+            }
+        } catch (e: ClassNotFoundError) {
+            return false
+        }
+        return true
+    }
+}
+
+inline fun BaseClassAndMethodCheck.runWithChecked(crossinline code:(Boolean) -> Unit) {
+    checkClassAndMethodExist().let { if (it) code(it) else null }
+}
--- app/src/main/java/com/yuk/miuihome/module/EnableBlurWhenOpenFolder.kt
+++ app/src/main/java/com/yuk/miuihome/module/EnableBlurWhenOpenFolder.kt
@@ -3,30 +3,42 @@ package com.yuk.miuihome.module
 import com.yuk.miuihome.utils.OwnSP
 import com.yuk.miuihome.utils.ktx.setReturnConstant
 
-class EnableBlurWhenOpenFolder {
+class EnableBlurWhenOpenFolder: BaseClassAndMethodCheck {
+
+    companion object {
+        var checked = true
+    }
 
     fun init() {
-        if (OwnSP.ownSP.getBoolean("simpleAnimation", false) and OwnSP.ownSP.getBoolean(
-                "testUser",
-                false
-            )
-        ) {
-            "com.miui.home.launcher.common.BlurUtils".setReturnConstant(
-                "isUserBlurWhenOpenFolder",
-                result = false
-            )
-        } else {
-            if (OwnSP.ownSP.getBoolean("blurWhenOpenFolder", false)) {
-                "com.miui.home.launcher.common.BlurUtils".setReturnConstant(
-                    "isUserBlurWhenOpenFolder",
-                    result = true
+        runWithChecked {
+            checked = it
+            if (OwnSP.ownSP.getBoolean("simpleAnimation", false) and OwnSP.ownSP.getBoolean(
+                    "testUser",
+                    false
                 )
-            } else {
+            ) {
                 "com.miui.home.launcher.common.BlurUtils".setReturnConstant(
                     "isUserBlurWhenOpenFolder",
                     result = false
                 )
+            } else {
+                if (OwnSP.ownSP.getBoolean("blurWhenOpenFolder", false)) {
+                    "com.miui.home.launcher.common.BlurUtils".setReturnConstant(
+                        "isUserBlurWhenOpenFolder",
+                        result = true
+                    )
+                } else {
+                    "com.miui.home.launcher.common.BlurUtils".setReturnConstant(
+                        "isUserBlurWhenOpenFolder",
+                        result = false
+                    )
+                }
             }
         }
     }
+
+    override fun classAndMethodList(): ArrayList<String> = arrayListOf(
+        "com.miui.home.launcher.common.BlurUtils", "isUserBlurWhenOpenFolder"
+    )
+
 }
