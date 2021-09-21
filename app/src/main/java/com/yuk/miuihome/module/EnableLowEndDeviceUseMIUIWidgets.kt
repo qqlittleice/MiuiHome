e4648cc79797baa32e64af7dcaba609ceae4d942
YuKongA <70465933+YuKongA@users.noreply.github.com>
2021-09-21 21:30:19 +0800

    分离功能

---
+++ app/src/main/java/com/yuk/miuihome/module/EnableLowEndDeviceUseMIUIWidgets.kt
@@ -0,0 +1,20 @@
+package com.yuk.miuihome.module
+
+import com.yuk.miuihome.utils.OwnSP
+import com.yuk.miuihome.utils.ktx.setReturnConstant
+
+class EnableLowEndDeviceUseMIUIWidgets {
+
+    fun init() {
+        if (OwnSP.ownSP.getBoolean("useMIUIWidgets", false)) {
+            "com.miui.home.launcher.MIUIWidgetUtil".setReturnConstant(
+                "isMIUIWidgetSupport",
+                result = true
+            )
+            "com.miui.home.launcher.DeviceConfig".setReturnConstant(
+                "isRecommendServerEnable",
+                result = false
+            )
+        }
+    }
+}
--- app/src/main/java/com/yuk/miuihome/module/SetDeviceLevel.kt
+++ app/src/main/java/com/yuk/miuihome/module/SetDeviceLevel.kt
@@ -1,35 +1,33 @@
 package com.yuk.miuihome.module
 
+import com.yuk.miuihome.utils.LogUtil
 import com.yuk.miuihome.utils.ktx.setReturnConstant
 
 class SetDeviceLevel {
 
     fun init() {
-        "com.miui.home.launcher.common.DeviceLevelUtils".setReturnConstant(
-            "getDeviceLevel",
-            result = 2
-        )
-        "com.miui.home.launcher.common.DeviceLevelUtils".setReturnConstant(
-            "getQualcommCpuLevel",
-            String::class.java,
-            result = 2
-        )
-        "com.mi.mibridge.DeviceLevel".setReturnConstant(
-            "getDeviceLevel",
-            result = 2
-        )
-        "com.miui.home.launcher.common.CpuLevelUtils".setReturnConstant(
-            "getQualcommCpuLevel",
-            String::class.java,
-            result = 2
-        )
-        "com.miui.home.launcher.MIUIWidgetUtil".setReturnConstant(
-            "isMIUIWidgetSupport",
-            result = true
-        )
-        "com.miui.home.launcher.DeviceConfig".setReturnConstant(
-            "isRecommendServerEnable",
-            result = false
-        )
+        try {
+            "com.miui.home.launcher.common.DeviceLevelUtils".setReturnConstant(
+                "getDeviceLevel",
+                result = 2
+            )
+            "com.miui.home.launcher.common.DeviceLevelUtils".setReturnConstant(
+                "getQualcommCpuLevel",
+                String::class.java,
+                result = 2
+            )
+            "com.mi.mibridge.DeviceLevel".setReturnConstant(
+                "getDeviceLevel",
+                result = 2
+            )
+            "com.miui.home.launcher.common.CpuLevelUtils".setReturnConstant(
+                "getQualcommCpuLevel",
+                String::class.java,
+                result = 2
+            )
+        } catch (e: Throwable) {
+            LogUtil.e(e)
+            throw e
+        }
     }
 }
