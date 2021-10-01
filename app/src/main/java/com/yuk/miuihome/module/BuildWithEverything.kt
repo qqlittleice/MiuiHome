f95e8dbc99799c0035bcbd3c392d261292760222
QQ littleice <1767523953@qq.com>
2021-10-01 18:34:12 +0800

    fix build

---
+++ app/src/main/java/com/yuk/miuihome/module/BuildWithEverything.kt
@@ -0,0 +1,28 @@
+package com.yuk.miuihome.module
+
+import android.widget.Toast
+import com.yuk.miuihome.Config
+import com.yuk.miuihome.HomeContext
+import java.io.*
+
+class BuildWithEverything {
+
+    private fun readStream(input: InputStream) {
+        val reader = BufferedReader(InputStreamReader(input))
+        var read = ""
+        while (true) {
+            val temp: String = reader.readLine() ?: break
+            read += temp
+        }
+        Toast.makeText(HomeContext.context, read, Toast.LENGTH_LONG).show()
+    }
+
+    fun init() {
+        try {
+            readStream(Runtime.getRuntime().exec("cmd package compile -m everything ${Config.hookPackage}").inputStream)
+        } catch (e: IOException) {
+            Toast.makeText(HomeContext.context, e.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
