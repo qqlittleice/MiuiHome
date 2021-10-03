2df1a0a844ae1aea7a6be2573f2518d58bd1602a
YuKongA <70465933+YuKongA@users.noreply.github.com>
2021-10-03 10:33:57 +0800

    nothing

--- app/src/main/java/com/yuk/miuihome/activity/EntryActivity.kt
+++ app/src/main/java/com/yuk/miuihome/activity/EntryActivity.kt
@@ -2,21 +2,18 @@ package com.yuk.miuihome.activity
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import com.yuk.miuihome.R
 import kotlin.concurrent.thread
 
 class EntryActivity : Activity() {
 
-    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(android.R.style.ThemeOverlay_DeviceDefault_Accent_DayNight)
+        setTheme(android.R.style.Theme_DeviceDefault_Settings)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.entry_activity)
         thread {
-            Thread.sleep(1200)
+            Thread.sleep(1000)
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
