985041c76876d8646313ba9a65872d2437dc8bff
QQ littleice <1767523953@qq.com>
2021-10-07 23:01:39 +0800

    XposedBridge Logger(debug only)

---
+++ app/src/main/java/com/yuk/miuihome/Logger.kt
@@ -0,0 +1,28 @@
+package com.yuk.miuihome
+
+import android.util.Log
+import de.robv.android.xposed.XposedBridge
+
+object Logger {
+
+    private const val maxLength = 4000
+
+    @JvmStatic
+    fun log(obj: Any) {
+        val content = if (obj is Throwable) Log.getStackTraceString(obj) else obj.toString()
+        if (content.length > maxLength) {
+            val chunkCount = content.length / maxLength
+            for (i in 0..chunkCount) {
+                val max = 4000 * (i + 1)
+                if (max >= content.length) {
+                    XposedBridge.log(content.substring(maxLength * i))
+                } else {
+                    XposedBridge.log(content.substring(maxLength * i, max))
+                }
+            }
+        } else {
+            XposedBridge.log(content)
+        }
+    }
+
+}
