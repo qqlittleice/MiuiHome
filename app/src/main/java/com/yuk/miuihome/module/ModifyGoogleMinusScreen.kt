17af9f0487b5655e7749eee771434aa64aaa8f70
YuKongA <70465933+YuKongA@users.noreply.github.com>
2021-10-18 18:21:06 +0800

    try again

--- app/src/main/java/com/yuk/miuihome/module/ModifyGoogleMinusScreen.kt
+++ app/src/main/java/com/yuk/miuihome/module/ModifyGoogleMinusScreen.kt
@@ -10,22 +10,14 @@ class ModifyGoogleMinusScreen {
 
     fun init() {
         if (OwnSP.ownSP.getBoolean("googleMinusScreen", false)) {
-            "com.miui.home.launcher.DeviceConfig".setReturnConstant(
-                "isUseGoogleMinusScreen",
-                result = true
-            )
             val configCls = "com.miui.home.launcher.DeviceConfig".findClass()
             val selectMinusScreenClientId =
                 configCls.getStaticObjectField("SELECT_MINUS_SCREEN_CLIENT_ID") as HashSet<String>
             selectMinusScreenClientId.add("")
             "com.miui.home.launcher.DeviceConfig".findClass().setBooleanField(
-                "CAN_SWITCH_MINUS_SCREEN", true
-            )
-            "com.miui.home.launcher.DeviceConfig".findClass().setBooleanField(
-                "ONLY_USE_GOOGLE_MINUS_SCREEN", false
+                "ONLY_USE_GOOGLE_MINUS_SCREEN", true
             )
             val appCls = "com.miui.home.launcher.Application".findClass()
-
             val app = XposedHelpers.callStaticMethod(appCls, "getInstance") as Application
             "com.miui.home.launcher.DeviceConfig".findClass().setBooleanField(
                 "IS_USE_GOOGLE_MINUS_SCREEN",
@@ -33,6 +25,10 @@ class ModifyGoogleMinusScreen {
                     app.contentResolver,
                     "switch_personal_assistant"
                 )
+            )
+            "com.miui.home.launcher.DeviceConfig".setReturnConstant(
+                "isUseGoogleMinusScreen",
+                result = true
             )
         }
     }
