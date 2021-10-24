0751cc060222dd9bac777064bcb4cb83dea365cb
YuKongA <70465933+YuKongA@users.noreply.github.com>
2021-10-24 19:25:40 +0800

    Update BuildWithEverything.kt

--- app/src/main/java/com/yuk/miuihome/module/BuildWithEverything.kt
+++ app/src/main/java/com/yuk/miuihome/module/BuildWithEverything.kt
@@ -11,20 +11,21 @@ import java.io.InputStreamReader
 
 class BuildWithEverything {
 
-    fun init() {
-        fun readStream(input: InputStream) {
-            val reader = BufferedReader(InputStreamReader(input))
-            var read = ""
-            while (true) {
-                val temp: String = reader.readLine() ?: break
-                read += temp
-            }
-            Toast.makeText(HomeContext.context, read, Toast.LENGTH_LONG).show()
+    private fun readStream(input: InputStream) {
+        val reader = BufferedReader(InputStreamReader(input))
+        var read = ""
+        while (true) {
+            val temp: String = reader.readLine() ?: break
+            read += temp
         }
+        Toast.makeText(HomeContext.context, read, Toast.LENGTH_LONG).show()
+    }
+
+    fun init() {
         try {
             readStream(
                 Runtime.getRuntime()
-                    .exec("cmd package compile -m everything ${Config.hookPackage}").inputStream
+                    .exec("su -c cmd package compile -m everything ${Config.hookPackage}").inputStream
             )
         } catch (e: IOException) {
             Toast.makeText(HomeContext.context, e.toString(), Toast.LENGTH_LONG).show()
