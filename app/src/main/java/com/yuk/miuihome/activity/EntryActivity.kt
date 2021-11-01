23b4d5b6b29ded4d3ac80c116a0c765c80d94a88
YuKongA <70465933+YuKongA@users.noreply.github.com>
2021-11-01 14:36:38 +0800

    上班摸鱼差点被抓..

--- app/src/main/java/com/yuk/miuihome/activity/EntryActivity.kt
+++ app/src/main/java/com/yuk/miuihome/activity/EntryActivity.kt
@@ -9,18 +9,12 @@ import android.view.Window
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.*
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -64,17 +58,13 @@ class EntryActivity : ComponentActivity() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Column(
-                modifier = Modifier
-                    .size(width = 140.dp, height = 170.dp)
-                    .padding(5.dp)
+                modifier = Modifier.size(width = 100.dp, height = 100.dp)
             ) {
                 Image(
                     painter = painterResource(R.drawable.icon),
                     contentDescription = "icon"
                 )
-                Text(stringResource(R.string.app_name), fontSize = 28.sp)
             }
-
         }
         Column(
             modifier = Modifier
@@ -83,12 +73,13 @@ class EntryActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(
+            Column(
                 Modifier
-                    .padding(0.dp)
-                    .padding(40.dp)
+                    .padding(0.dp, 100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Hook for MIUI Launcher", fontSize = 18.sp)
+                Text(stringResource(R.string.app_name), fontSize = 28.sp)
+                Text("Hook for MIUI Launcher", fontSize = 12.sp)
             }
         }
     }
