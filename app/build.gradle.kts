3465f3e4d5701c632ece93325b1076bd005bb1f8
YuKongA <1348547200@qq.com>
2021-11-24 17:43:52 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
             if (appVariant is ApplicationVariantImpl) appVariant
             else (appVariant as AnalyticsEnabledApplicationVariant).delegate as ApplicationVariantImpl
         variant.outputs.forEach {
-            it.outputFileName.set("MiuiHome_${verName}(${verCode}).apk")
+            if  (appVariant.buildType == "release") it.outputFileName.set("MiuiHome_${verName}(${verCode})_release.apk") else it.outputFileName.set("MiuiHome_${verName}(${verCode})_debug.apk")
         }
     }
 
