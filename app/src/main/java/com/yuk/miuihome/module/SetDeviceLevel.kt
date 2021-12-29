eb1d715b7f96b3a5340526f1186ef629c80ac87e
YuKongA <1348547200@qq.com>
2021-12-29 15:30:40 +0800

    为 MIUI 13 前设备开启无字模式功能
    * 需桌面支持，目前仅支持从最新开发版提取的桌面

--- app/src/main/java/com/yuk/miuihome/module/SetDeviceLevel.kt
+++ app/src/main/java/com/yuk/miuihome/module/SetDeviceLevel.kt
@@ -3,7 +3,7 @@ package com.yuk.miuihome.module
 import com.yuk.miuihome.utils.LogUtil
 import com.yuk.miuihome.utils.ktx.setReturnConstant
 
-class SetDeviceLevel {
+class SetDeviceLevel : BaseClassAndMethodCheck {
 
     fun init() {
         try {
@@ -13,8 +13,15 @@ class SetDeviceLevel {
             "com.miui.home.launcher.common.DeviceLevelUtils".setReturnConstant("isLowLevelOrLiteDevice", result = false)
             "com.miui.home.launcher.DeviceConfig".setReturnConstant("isDefaultIcon", result = true)
             "com.miui.home.launcher.DeviceConfig".setReturnConstant("isMiuiLiteVersion", result = false)
+            runWithChecked {
+                "com.miui.home.launcher.util.noword.NoWordSettingHelperKt".setReturnConstant("isNoWordAvailable", result = true)
+            }
         } catch (e: Throwable) {
             LogUtil.e(e)
         }
     }
+
+    override fun classAndMethodList(): ArrayList<String> = arrayListOf(
+        "com.miui.home.launcher.util.noword.NoWordSettingHelperKt", "isNoWordAvailable"
+    )
 }
