94cfcfe49038a88a3677715f76b1667e72e3d196
YuKongA <1348547200@qq.com>
2022-01-05 11:19:38 +0800

    try fix IconTitleFontSize

--- app/src/main/java/com/yuk/miuihome/module/ModifyIconTitleFontSize.kt
+++ app/src/main/java/com/yuk/miuihome/module/ModifyIconTitleFontSize.kt
@@ -2,12 +2,12 @@ package com.yuk.miuihome.module
 
 import android.content.Context
 import android.util.TypedValue
-import android.view.ContentInfo
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.yuk.miuihome.XposedInit.Companion.hasHookPackageResources
 import com.yuk.miuihome.utils.Config
+import com.yuk.miuihome.utils.LogUtil
 import com.yuk.miuihome.utils.OwnSP
 import com.yuk.miuihome.utils.ktx.callMethod
 import com.yuk.miuihome.utils.ktx.findClass
@@ -22,54 +22,65 @@ class ModifyIconTitleFontSize {
         val launcherClass = "com.miui.home.launcher.Launcher".findClass()
         val shortcutInfoClass = "com.miui.home.launcher.ShortcutInfo".findClass()
         if (value == -1f || value == 12f) return
-        "com.miui.home.launcher.ItemIcon".hookAfterMethod("onFinishInflate"
-        ) {
-            val mTitle = it.thisObject.getObjectField("mTitle") as TextView
-            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
-        }
-        "com.miui.home.launcher.maml.MaMlWidgetView".hookAfterMethod("onFinishInflate"
-        ) {
-            val mTitle = it.thisObject.getObjectField("mTitleTextView") as TextView
-            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
-        }
-        "com.miui.home.launcher.LauncherMtzGadgetView".hookAfterMethod("onFinishInflate"
-        ) {
-            val mTitle = it.thisObject.getObjectField("mTitleTextView") as TextView
-            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
-        }
-        "com.miui.home.launcher.LauncherWidgetView".hookAfterMethod("onFinishInflate"
-        ) {
-            val mTitle = it.thisObject.getObjectField("mTitleTextView") as TextView
-            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
-        }
-        "com.miui.home.launcher.ShortcutIcon".hookAfterMethod("fromXml",
-            Int::class.javaPrimitiveType,
-            launcherClass,
-            ViewGroup::class.java,
-            shortcutInfoClass
+        try {
+            "com.miui.home.launcher.ItemIcon".hookAfterMethod(
+                "onFinishInflate"
             ) {
-            val buddyIconView = it.args[3].callMethod("getBuddyIconView", it.args[2]) as View
-            val mTitle = buddyIconView.getObjectField("mTitle") as TextView
-            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
-        }
-        "com.miui.home.launcher.ShortcutIcon".hookAfterMethod("createShortcutIcon",
-            Int::class.javaPrimitiveType,
-            launcherClass,
-            ViewGroup::class.java
-        ) {
-            val buddyIcon = it.result as View
-            val mTitle = buddyIcon.getObjectField("mTitle") as TextView
-            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
-        }
-        "com.miui.home.launcher.common.Utilities".hookAfterMethod("adaptTitleStyleToWallpaper",
-            Context::class.java,
-            TextView::class.java,
-            Int::class.javaPrimitiveType,
-            Int::class.javaPrimitiveType
-        ) {
-            val mTitle = it.args[1] as TextView
-            if (mTitle.id == mTitle.resources.getIdentifier("icon_title", "id", Config.hookPackage))
+                val mTitle = it.thisObject.getObjectField("mTitle") as TextView
                 mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+            "com.miui.home.launcher.maml.MaMlWidgetView".hookAfterMethod(
+                "onFinishInflate"
+            ) {
+                val mTitle = it.thisObject.getObjectField("mTitleTextView") as TextView
+                mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+            "com.miui.home.launcher.LauncherMtzGadgetView".hookAfterMethod(
+                "onFinishInflate"
+            ) {
+                val mTitle = it.thisObject.getObjectField("mTitleTextView") as TextView
+                mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+            "com.miui.home.launcher.LauncherWidgetView".hookAfterMethod(
+                "onFinishInflate"
+            ) {
+                val mTitle = it.thisObject.getObjectField("mTitleTextView") as TextView
+                mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+            "com.miui.home.launcher.ShortcutIcon".hookAfterMethod(
+                "fromXml",
+                Int::class.javaPrimitiveType,
+                launcherClass,
+                ViewGroup::class.java,
+                shortcutInfoClass
+            ) {
+                val buddyIconView = it.args[3].callMethod("getBuddyIconView", it.args[2]) as View
+                val mTitle = buddyIconView.getObjectField("mTitle") as TextView
+                mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+            "com.miui.home.launcher.ShortcutIcon".hookAfterMethod(
+                "createShortcutIcon",
+                Int::class.javaPrimitiveType,
+                launcherClass,
+                ViewGroup::class.java
+            ) {
+                val buddyIcon = it.result as View
+                val mTitle = buddyIcon.getObjectField("mTitle") as TextView
+                mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+            "com.miui.home.launcher.common.Utilities".hookAfterMethod(
+                "adaptTitleStyleToWallpaper",
+                Context::class.java,
+                TextView::class.java,
+                Int::class.javaPrimitiveType,
+                Int::class.javaPrimitiveType
+            ) {
+                val mTitle = it.args[1] as TextView
+                if (mTitle.id == mTitle.resources.getIdentifier("icon_title", "id", Config.hookPackage))
+                    mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, value)
+            }
+        } catch (e: Throwable) {
+            LogUtil.e(e)
         }
     }
 }
