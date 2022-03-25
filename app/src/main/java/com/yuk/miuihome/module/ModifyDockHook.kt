058cf671b3a9d7b5eb2affeda2ee1616edeea102
方块君 <fkj@fkj233.cn>
2022-03-26 00:35:45 +0800

    独立BlurView

--- app/src/main/java/com/yuk/miuihome/module/ModifyDockHook.kt
+++ app/src/main/java/com/yuk/miuihome/module/ModifyDockHook.kt
@@ -2,24 +2,19 @@ package com.yuk.miuihome.module
 
 import android.content.Context
 import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
-import android.view.View
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
 import com.github.kyuubiran.ezxhelper.init.InitFields.appContext
 import com.github.kyuubiran.ezxhelper.utils.Log
-import com.github.kyuubiran.ezxhelper.utils.getMethodByClassOrObject
 import com.yuk.miuihome.utils.OwnSP
 import com.yuk.miuihome.utils.ktx.*
+import com.yuk.miuihome.view.BlurFrameLayout
 import de.robv.android.xposed.XposedHelpers
 
 class ModifyDockHook {
-
-    var blur: FrameLayout? = null
-    var init = false
 
     fun init() {
         if (!OwnSP.ownSP.getBoolean("dockSettings", false)) return
@@ -66,7 +61,6 @@ class ModifyDockHook {
                     else -> return@hookAfterMethod
                 }
                 background.cornerRadius = dp2px((OwnSP.ownSP.getFloat("dockRadius", 2.5f) * 10)).toFloat()
-                if (!init && OwnSP.ownSP.getBoolean("searchBarBlur", false)) blur?.let { it1 -> setViewBlurForS(it1) }
                 background.setStroke(0, 0)
                 when (searchBarDesktop) {
                     is RippleDrawable -> searchBarDesktop.setDrawable(0, background)
@@ -89,10 +83,12 @@ class ModifyDockHook {
                 // 添加模糊
                 if (OwnSP.ownSP.getBoolean("searchBarBlur", false)) {
                     searchBarObject.removeAllViews()
-                    blur = FrameLayout(searchBarObject.context)
-                    blur?.addView(searchBarDesktop)
+                    val blur = BlurFrameLayout(searchBarObject.context)
+                    blur.setBlurRadius(100)
+                    blur.setCornerRadius(dp2px((OwnSP.ownSP.getFloat("dockRadius", 2.5f) * 10)).toFloat())
+                    blur.setColor(Color.parseColor("#33626262"))
+                    blur.addView(searchBarDesktop)
                     searchBarObject.addView(blur)
-
                 }
                 // 修改应用列表搜索框
                 val mAllAppViewField = launcherClass.getDeclaredField("mAppsView")
@@ -111,16 +107,5 @@ class ModifyDockHook {
         } catch (e: XposedHelpers.ClassNotFoundError) {
             Log.ex(e)
         }
-    }
-
-    private fun setViewBlurForS(view: View) {
-        val viewRootImplMethod = view.getMethodByClassOrObject("getViewRootImpl")
-        val viewRootImpl = viewRootImplMethod.invoke(view)
-        if (viewRootImpl != null) init = true
-        val drawable = viewRootImpl?.callMethod("createBackgroundBlurDrawable")
-        drawable?.callMethod("setBlurRadius", 100)
-        drawable?.callMethod("setCornerRadius", dp2px((OwnSP.ownSP.getFloat("dockRadius", 2.5f) * 10)).toFloat())
-        drawable?.callMethod("setColor", Color.parseColor("#33626262"))
-        view.background = drawable as? Drawable
     }
 }
