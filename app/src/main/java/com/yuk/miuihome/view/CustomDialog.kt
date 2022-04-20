85b8a8cd7c5e02d5ec77181cd3e65b79154dfcf3
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-04-20 13:19:17 +0800

    简单优化 Dialog 效果

--- app/src/main/java/com/yuk/miuihome/view/CustomDialog.kt
+++ app/src/main/java/com/yuk/miuihome/view/CustomDialog.kt
@@ -2,6 +2,8 @@ package com.yuk.miuihome.view
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Build
 import android.view.*
 import android.widget.Button
@@ -11,18 +13,25 @@ import android.widget.TextView
 import com.yuk.miuihome.R
 import com.yuk.miuihome.utils.ktx.dp2px
 import com.yuk.miuihome.utils.ktx.getDensityDpi
+import com.yuk.miuihome.utils.ktx.isDarkMode
 
-class CustomDialog(context: Context) : Dialog(context, R.style.CustomDialog) {
+class CustomDialog(context: Context) : Dialog(context) {
     var view: View
 
     init {
         if (getDensityDpi() > 440) {
             view = createView(context, R.layout.dialog_pad_layout)
             window!!.attributes.width = dp2px(380f)
+            val radius = floatArrayOf(dp2px(25f).toFloat(), dp2px(25f).toFloat(), dp2px(25f).toFloat(), dp2px(25f).toFloat())
+            val background = createDrawable(Color.parseColor(if (isDarkMode()) "#242424" else "#FFFFFF"), radius)
+            view.background = background
             window!!.setGravity(Gravity.CENTER)
         }
         else {
             view = createView(context, R.layout.dialog_layout)
+            val radius = floatArrayOf(dp2px(25f).toFloat(), dp2px(25f).toFloat(), 0f, 0f)
+            val background = createDrawable(Color.parseColor(if (isDarkMode()) "#242424" else "#FFFFFF"), radius)
+            view.background = background
             window!!.attributes.width = WindowManager.LayoutParams.MATCH_PARENT
             window!!.setGravity(Gravity.BOTTOM)
         }
@@ -162,6 +171,7 @@ class CustomDialog(context: Context) : Dialog(context, R.style.CustomDialog) {
     fun getEditText(): String = view.findViewById<EditText>(R.id.EditText).text.toString()
 
     override fun show() {
+        super.show()
         val lp = window!!.attributes
         if (Build.VERSION.SDK_INT >= 31) {
             window!!.addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND)
@@ -172,6 +182,21 @@ class CustomDialog(context: Context) : Dialog(context, R.style.CustomDialog) {
             lp.dimAmount = 0.3f
             window!!.attributes = lp
         }
-        super.show()
+
+    }
+
+    private fun createDrawable(color: Int, radius: FloatArray): GradientDrawable {
+        return try {
+            GradientDrawable().apply {
+                shape = GradientDrawable.RECTANGLE
+                setColor(color)
+                setStroke(0, 0)
+                if (radius.size == 4) {
+                    cornerRadii = floatArrayOf(radius[0], radius[0], radius[1], radius[1], radius[2], radius[2], radius[3], radius[3])
+                }
+            }
+        } catch (e: Exception) {
+            GradientDrawable()
+        }
     }
 }
