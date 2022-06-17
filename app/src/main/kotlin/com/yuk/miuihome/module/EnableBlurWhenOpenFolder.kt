4ea7ca8f82b0d24a5462006e1aa38bdcb7c3d4c6
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-06-17 20:14:19 +0800

    将 应用返回模糊 加回
    * 同时修复 侧滑返回桌面时返回模糊不生效的问题
    * 同时修复 上个 ci 版本针对 release 桌面文件夹模糊完善后，从最近任务返回文件夹时模糊依旧消失的问题

--- app/src/main/kotlin/com/yuk/miuihome/module/EnableBlurWhenOpenFolder.kt
+++ app/src/main/kotlin/com/yuk/miuihome/module/EnableBlurWhenOpenFolder.kt
@@ -28,10 +28,11 @@ class EnableBlurWhenOpenFolder {
                         val blurClass = "com.miui.home.launcher.common.BlurUtils".findClass()
                         val folderInfo = "com.miui.home.launcher.FolderInfo".findClass()
                         val launcherClass = "com.miui.home.launcher.Launcher".findClass()
+                        val launcherStateClass = "com.miui.home.launcher.LauncherState".findClass()
                         launcherClass.hookAfterMethod("onCreate", Bundle::class.java) {
                             val activity = it.thisObject as Activity
                             var isFolderShowing = false
-                                launcherClass.hookAfterMethod("isFolderShowing") { hookParam ->
+                            launcherClass.hookAfterMethod("isFolderShowing") { hookParam ->
                                 isFolderShowing = hookParam.result as Boolean
                             }
                             launcherClass.hookAfterMethod("openFolder", folderInfo, View::class.java) {
@@ -44,6 +45,9 @@ class EnableBlurWhenOpenFolder {
                                 if (isFolderShowing) hookParam.result = blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true)
                             }
                             blurClass.hookAfterMethod("fastBlurWhenFinishOpenOrCloseApp", launcherClass) { hookParam ->
+                                if (isFolderShowing) hookParam.result = blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true, 0L)
+                            }
+                            blurClass.hookAfterMethod("fastBlurWhenExitRecents", launcherClass, launcherStateClass, Boolean::class.java) { hookParam ->
                                 if (isFolderShowing) hookParam.result = blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true, 0L)
                             }
                             launcherClass.hookAfterMethod("onGesturePerformAppToHome") {
--- app/src/main/kotlin/com/yuk/miuihome/view/data/DataHelper.kt
+++ app/src/main/kotlin/com/yuk/miuihome/view/data/DataHelper.kt
@@ -163,6 +163,7 @@ object DataHelper {
 
             add(Item(list = arrayListOf(SubtitleV(resId = R.string.TestFeature))))
             add(Item(list = arrayListOf(TextWithSwitchV(TextWithSummaryV(titleResId = R.string.SimpleAnimation), "simpleAnimation", customOnCheckedChangeListener =  { _, _ -> currentActivity.recreate() }))))
+            if (!OwnSP.ownSP.getBoolean("alwaysBlur", false)) add(Item(list = arrayListOf(TextWithSwitchV(TextWithSummaryV(titleResId = R.string.AppReturnAmin), "appReturnAmin"))))
             add(Item(list = arrayListOf(TextWithSwitchV(TextWithSummaryV(titleResId = R.string.InfiniteScroll), "infiniteScroll"))))
             add(Item(list = arrayListOf(TextWithSwitchV(TextWithSummaryV(titleResId = R.string.RecommendServer), "recommendServer"))))
             add(Item(list = arrayListOf(TextWithSwitchV(TextWithSummaryV(titleResId = R.string.HideSeekPoints), "hideSeekPoints"))))
