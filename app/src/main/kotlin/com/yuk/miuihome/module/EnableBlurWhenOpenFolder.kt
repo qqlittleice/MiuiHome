506f589a860e6554bfabbe6e8c278366cb4da6a2
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-06-17 19:19:01 +0800

    为 release 版本桌面构建更完美的文件夹模糊体验

--- app/src/main/kotlin/com/yuk/miuihome/module/EnableBlurWhenOpenFolder.kt
+++ app/src/main/kotlin/com/yuk/miuihome/module/EnableBlurWhenOpenFolder.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.view.View
 import com.yuk.miuihome.XposedInit
 import com.yuk.miuihome.utils.OwnSP
-import com.yuk.miuihome.utils.ktx.callStaticMethod
-import com.yuk.miuihome.utils.ktx.findClass
-import com.yuk.miuihome.utils.ktx.hookAfterMethod
-import com.yuk.miuihome.utils.ktx.hookBeforeMethod
+import com.yuk.miuihome.utils.ktx.*
 
 class EnableBlurWhenOpenFolder {
 
@@ -31,14 +28,26 @@ class EnableBlurWhenOpenFolder {
                         val blurClass = "com.miui.home.launcher.common.BlurUtils".findClass()
                         val folderInfo = "com.miui.home.launcher.FolderInfo".findClass()
                         val launcherClass = "com.miui.home.launcher.Launcher".findClass()
-                        launcherClass.hookAfterMethod("onCreate", Bundle::class.java
-                        ) {
+                        launcherClass.hookAfterMethod("onCreate", Bundle::class.java) {
                             val activity = it.thisObject as Activity
+                            var isFolderShowing = false
+                                launcherClass.hookAfterMethod("isFolderShowing") { hookParam ->
+                                isFolderShowing = hookParam.result as Boolean
+                            }
                             launcherClass.hookAfterMethod("openFolder", folderInfo, View::class.java) {
                                 blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true)
                             }
                             launcherClass.hookAfterMethod("closeFolder", Boolean::class.java) {
                                 blurClass.callStaticMethod("fastBlur", 0.0f, activity.window, true)
+                            }
+                            blurClass.hookAfterMethod("fastBlurWhenStartOpenOrCloseApp", Boolean::class.java, launcherClass) { hookParam ->
+                                if (isFolderShowing) hookParam.result = blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true)
+                            }
+                            blurClass.hookAfterMethod("fastBlurWhenFinishOpenOrCloseApp", launcherClass) { hookParam ->
+                                if (isFolderShowing) hookParam.result = blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true, 0L)
+                            }
+                            launcherClass.hookAfterMethod("onGesturePerformAppToHome") {
+                                if (isFolderShowing) blurClass.callStaticMethod("fastBlur", 1.0f, activity.window, true, 0L)
                             }
                         }
                     }
